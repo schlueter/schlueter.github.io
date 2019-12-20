5250e41bef12c7781822def624220b83698d4da9
Schlueter <b@schlueter.blue>
2019-12-20 17:26:53 -0500

    Strip js and html

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 (function () {
-    function removeBareText(elem) {
-        function textNodes() {
-            return this.nodeType == 3
-        }
-        elem.contents()
-            .filter(textNodes)
-            .remove()
-    }
-    function onReady() {
-        removeBareText($('.container'))
-    }
-    $(window).ready(onReady())
+
 })()
